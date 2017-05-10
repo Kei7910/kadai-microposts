31f0328257e8413b947940812b8ffeabe4b87e73
Keisuke Nagai <yee_7798@yahoo.co.jp>
2017-05-10 14:52:26 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-  
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.all.page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
   end
 
   def new
@@ -29,3 +31,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+end
